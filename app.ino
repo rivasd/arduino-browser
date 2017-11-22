915cfccc9cdb3994c2e2c1011fd07076540975e2
rivasd <rivasdaniel1992@gmail.com>
2017-11-21 19:38:34 -0500

    starting true latency measuring code

--- app.ino
+++ app.ino
@@ -3,10 +3,14 @@ char msg = 'a';
 const int lightPin  = 0;
 int light           = 0;
 
+int lptPin = 2;
+
+unsigned long latency;
 
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(lptPin, INPUT);
     digitalWrite(LED_BUILTIN, LOW);
 
     Serial.begin(9600);
@@ -15,13 +19,29 @@ void setup()
 void loop()
 {
 
-    light = analogRead(lightPin);
+    //light = analogRead(lightPin);
 
     if(Serial.available()){
         msg = Serial.read();
         if(msg == 'r'){
-            Serial.write('r');
+            //we should start timing
+            unsigned long start = micros();
+            while(1){
+                if(digitalRead(lptPin)){
+                    //we got a signal! calculate delay
+                    latency = micros() - start;
+                    //send to computer
+                    Serial.print(latency);
+                    delay(5); //wait out the time to trigger to end
+                    break;
+                }
+            }
         }
         digitalWrite(LED_BUILTIN, msg == 'a' ? LOW : HIGH);
+    }
+
+    if(digitalRead(lptPin)){
+        Serial.println("Trigger detected!");
+        delay(5);
     }
 }
